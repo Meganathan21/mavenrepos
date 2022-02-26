e058aa2ba1d4f7dfe877c8a97f577b77b568d43c
Meganathan <meganathan9566@gmail.com>
2022-02-26 18:37:55 +0530

    javafile

---
+++ Break.java
@@ -0,0 +1,13 @@
+package com.control;
+
+public class Break {
+public static void main(String[] args) {
+	for (int i = 1; i <=25 ; i++) {
+    if((i==12)||(i==15))	{
+		continue;
+	}
+    System.out.print(i +" ");
+    
+}
+}
+}
